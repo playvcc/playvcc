48750ae1b3e26a63ed8b3b3e75af784af507622a
Trozii <140472571+Trozii@users.noreply.github.com>
2026-05-14 20:45:06 -0500

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,2 +1,2 @@
-export const SUPABASE_URL = "https://supabase.com/dashboard/project/wvvnhjtaynkvczsepdsz/settings/api-keys";
+export const SUPABASE_URL = "https://wvvnhjtaynkvczsepdsz.supabase.co";
 export const SUPABASE_ANON_KEY = "sb_publishable_ZBvUMXNTtzAZgQmoNvgPpg_LmhifcZ1";
