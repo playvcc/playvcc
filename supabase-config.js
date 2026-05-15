b898928f8ac144cda08cf4fef7423552b087e0cf
Trozii <140472571+Trozii@users.noreply.github.com>
2026-05-14 19:32:08 -0500

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,4 +0,0 @@
-// VCC Supabase Config
-// Put your real Supabase project URL and anon key here.
-export const SUPABASE_URL = "PASTE_SUPABASE_URL_HERE";
-export const SUPABASE_ANON_KEY = "PASTE_SUPABASE_ANON_KEY_HERE";
