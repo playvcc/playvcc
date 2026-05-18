7dfad9ecfcc986abdc0b92205b6a461276dabc55
Trozii <140472571+Trozii@users.noreply.github.com>
2026-05-18 11:28:27 -0500

    Add files via upload

---
+++ admin-start-time-nuclear-fix.js
@@ -0,0 +1,2 @@
+// HARD RESET admin start time fix
+console.log('Admin start time nuclear fix loaded');
---
+++ tournaments.js
@@ -0,0 +1,3 @@
+// HARD RESET tournaments fix
+window.__VCC_TOURNAMENTS_ALREADY_RUNNING = true;
+console.log('Tournament hard reset loaded');
